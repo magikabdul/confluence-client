1555e5865950d4183e7527703143c67512f3c94c
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2024-03-31 15:21:16 +0200

    initial commit

---
+++ src/main/java/cloud/cholewa/confluence/ConfluenceClientApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.confluence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfluenceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfluenceClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/cholewa/confluence/ConfluenceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.confluence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfluenceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
