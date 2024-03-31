3364589e22c6b39354ad25eee1239de52204019b
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2024-03-31 23:35:06 +0200

    endline fix

--- src/main/java/cloud/cholewa/confluence/ConfluenceClientApplication.java
+++ src/main/java/cloud/cholewa/confluence/ConfluenceClientApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfluenceClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfluenceClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfluenceClientApplication.class, args);
+    }
 
 }
